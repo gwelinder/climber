378bca3cfcb9a0f55f039949adb8b3af33f7cf92
gwelinder <gwelinder@gmail.com>
2023-10-04 20:07:19 +0200

    Initial Commit

---
+++ climber/climberApp.swift
@@ -0,0 +1,17 @@
+//
+//  climberApp.swift
+//  climber
+//
+//  Created by Gustav Welinder on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct climberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
